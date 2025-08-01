1774dedb2fe5aeb26a32c87a22b47cb8bce52366
fransisca25 <fcel2510@gmail.com>
2025-08-01 13:59:17 +0900

    adding test edge not update item if the item is not in the cart

--- data/cart.js
+++ data/cart.js
@@ -97,6 +97,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
         } 
     });
 
+    if (!matchingItem) {
+        return;
+    }
+
     matchingItem.deliveryOptionId = deliveryOptionId;
 
     saveToStorage();
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,10 +1,13 @@
-import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart ,updateDeliveryOption } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
     const productId = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
 
-    it('adds an existing product to the cart', () => {
+    beforeEach(() => {
         spyOn(localStorage, 'setItem');
+    });
+
+    it('adds an existing product to the cart', () => {
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: productId, 
@@ -33,7 +36,6 @@ describe('test suite: addToCart', () => {
     });
 
     it('adds a new product to the cart', () => {
-        spyOn(localStorage, 'setItem');
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
@@ -61,10 +63,13 @@ describe('test suite: addToCart', () => {
 
 describe('test suite: removeFromCart', () => {
     const productId = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    const productIdOther = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+    const productIdNotExist = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+    });
 
     it('remove product from cart', () => {
-        spyOn(localStorage, 'setItem');
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: productId, 
@@ -81,7 +86,6 @@ describe('test suite: removeFromCart', () => {
     });
 
     it('not removing item if the item is not in the cart', () => {
-        spyOn(localStorage, 'setItem');
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: productId, 
@@ -91,9 +95,47 @@ describe('test suite: removeFromCart', () => {
         });
         loadFromStorage();
 
-        removeFromCart(productIdOther);
+        removeFromCart(productIdNotExist);
 
         expect(cart.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
+});
+
+
+describe('test suite: updateDeliveryOption', () => {
+    const productId = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productIdNotExist = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: productId, 
+                quantity: 1, 
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+    });
+
+    it('update the delivery option', () => {
+        updateDeliveryOption(productId, '3');
+
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(productId);
+        expect(cart[0].quantity).toEqual(1);
+        expect(cart[0].deliveryOptionId).toEqual('3');
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
+
+    it('not updating item if the item is not in the cart', () => {
+        updateDeliveryOption(productIdNotExist);
+
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(productId);
+        expect(cart[0].quantity).toEqual(1);
+        expect(cart[0].deliveryOptionId).toEqual('1');
+        expect(localStorage.setItem).toHaveBeenCalledTimes(0);
     });
 });
