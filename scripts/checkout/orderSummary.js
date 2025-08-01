6a1eee2b14fbbec1a91d0f5bc74ca67edb07e7ad
fransisca25 <fcel2510@gmail.com>
2025-08-01 12:47:02 +0900

    create automated test for order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,5 +1,5 @@
 import { calculateCartQuantity, cart, removeFromCart, updateQuantity, updateDeliveryOption } from '../../data/cart.js';
-import { products, getProduct } from '../../data/products.js';
+import { getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
@@ -22,7 +22,7 @@ export function renderOrderSummary() {
         const dateString = deliveryDate.format('dddd, MMMM D');
 
         cartSummaryHTML += `
-            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+            <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                 Delivery date: ${dateString}
                 </div>
@@ -38,7 +38,7 @@ export function renderOrderSummary() {
                     <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                     </div>
-                    <div class="product-quantity">
+                    <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                         Quantity: <span class="quantity-label js-label-quantity-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -49,7 +49,7 @@ export function renderOrderSummary() {
                     <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id=${matchingProduct.id}>
                     Save
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id=${matchingProduct.id}>
                         Delete
                     </span>
                     </div>
